512915ba15ebd002bd88bbcf8a46e374967f9e5c
Micah Lynn <micah@sabado.com>
2020-08-02 12:58:57 -0400

    Added preliminary pseudocode to ruby file.

---
+++ sub_string.rb
@@ -0,0 +1,6 @@
+# Create variable named "dictionary" and assign an array of strings
+# Create a method named "substrings" that accepts a string and an array
+    #Create an each loop that checks each dictionary value
+        #IF #includes? dictionary value
+            #prints dictionary value
+# Print the method with a given string and the dictionary variable
