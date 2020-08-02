81f5480cf8867f9c568d5c8681a084fdeb95996e
Micah Lynn <micah@sabado.com>
2020-08-02 13:57:42 -0400

    Program working by exporting result to array.

--- sub_string.rb
+++ sub_string.rb
@@ -1,5 +1,7 @@
+require 'colorize'
+
 #Prints variable that stores string to evaluate
-puts evaluate_string = "Hi, I am Bob. I am free later if you want to talk."
+puts evaluate_string = "Hi, I am Bob. I am free later if you want to talk.".yellow
 
 #Specified array of strings to search for
 dictionary = ['hello', 'hi', 'as', 'a', 'free', 'like', 'talk', 'on', 'top', 'less', 'the']
@@ -7,9 +9,9 @@ dictionary = ['hello', 'hi', 'as', 'a', 'free', 'like', 'talk', 'on', 'top', 'le
 #Defines a method that searches a string with the given array
 def substrings(string, array)
     string_includes_array = []
-    array.each do |n|
-        if string.downcase.include?(n)
-            string_includes_array += [n]
+    array.each do |word|
+        if string.downcase.include?(word)
+            string_includes_array += ["#{word} " + "=> " + "#{string.downcase.scan(word).length}"]
         end
     end
     print string_includes_array
