d7b638c6f31d8ad719fb8d91eda57a6d306a056a
Micah Lynn <micah@sabado.com>
2020-08-02 14:06:31 -0400

    Program working by exporting result to hash.

--- sub_string.rb
+++ sub_string.rb
@@ -6,15 +6,15 @@ puts evaluate_string = "Hi, I am Bob. I am free later if you want to talk.".yell
 #Specified array of strings to search for
 dictionary = ['hello', 'hi', 'as', 'a', 'free', 'like', 'talk', 'on', 'top', 'less', 'the']
 
-#Defines a method that searches a string with the given array
+#Defines a method that searches a string with the given array adn returns a hash
 def substrings(string, array)
-    string_includes_array = []
+    string_includes_hash = {}
     array.each do |word|
         if string.downcase.include?(word)
-            string_includes_array += ["#{word} " + "=> " + "#{string.downcase.scan(word).length}"]
+            string_includes_hash[word] = string.downcase.scan(word).length
         end
     end
-    print string_includes_array
+    print string_includes_hash
 end
 
 # Print the method with a given string and the dictionary variable
