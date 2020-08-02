e045a67b4c6166b6474f4bfd21f96b30e35db4a3
Micah Lynn <micah@sabado.com>
2020-08-02 13:23:57 -0400

    Specifies if dictionary word appears in evaluated string (does not work to count multiple word appearances)

--- sub_string.rb
+++ sub_string.rb
@@ -1,6 +1,19 @@
-# Create variable named "dictionary" and assign an array of strings
-# Create a method named "substrings" that accepts a string and an array
-    #Create an each loop that checks each dictionary value
-        #IF #includes? dictionary value
-            #prints dictionary value
+#Prints variable that stores string to evaluate
+puts evaluate_string = "Hi, I am Bob. I am free later if you want to talk."
+
+#Specified array of strings to search for
+dictionary = ['hello', 'hi', 'as', 'a', 'free', 'like', 'talk', 'on', 'top', 'less', 'the']
+
+#Defines a method that searches a string with the given array
+def substrings(string, array)
+    string_includes_array = []
+    array.each do |n|
+        if string.downcase.include?(n)
+            string_includes_array += [n]
+        end
+    end
+    print string_includes_array
+end
+
 # Print the method with a given string and the dictionary variable
+puts substrings(evaluate_string, dictionary)
